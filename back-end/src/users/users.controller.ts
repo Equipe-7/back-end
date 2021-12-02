ef68c5da143e06861da2268925b4895018dc24ff
lucasbanksys <lucs@live.jp>
2021-12-02 20:14:43 -0300

    implementação do service no user / inst do prisma validator

--- back-end/src/users/users.controller.ts
+++ back-end/src/users/users.controller.ts
@@ -1,4 +1,48 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { UserRole } from './enum/role.enum';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  createUser(@Body() data: CreateUserDto) {
+    return this.usersService.create(data, UserRole.USER);
+  }
+
+  @Post()
+  createAdmin(@Body() data: CreateUserDto) {
+    return this.usersService.create(data, UserRole.ADMIN);
+  }
+
+  @Get()
+  findAll() {
+    return this.usersService.findAll();
+  }
+
+  @Get(':username')
+  findOne(@Param('username') id: string) {
+    return this.usersService.findOne(id);
+  }
+
+  @Patch(':username')
+  update(@Param('username') id: string, @Body() updateUserDto: UpdateUserDto) {
+    return this.usersService.update(id, updateUserDto);
+  }
+
+  @Delete(':username')
+  remove(@Param('username') id: string) {
+    return this.usersService.remove(id);
+  }
+}
--- back-end/src/users/users.module.ts
+++ back-end/src/users/users.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
-import { UsersService } from './users.service';
+import { PrismaService } from 'src/prisma.service';
 
 @Module({
-  providers: [UsersService],
-  controllers: [UsersController]
+  controllers: [UsersController],
+  providers: [UsersService, PrismaService],
 })
 export class UsersModule {}
