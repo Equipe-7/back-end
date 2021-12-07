6741dcdff35d3b93bc481f23320f5f8d0b5e3d79
RudhyDev <rudhy.pereira@gmail.com>
2021-12-06 22:48:16 -0300

    controller pronto

--- src/companies/dto/create-company.dto.ts
+++ src/companies/dto/create-company.dto.ts
@@ -1,1 +1,18 @@
-export class CreateCompanyDto {}
+import { IsString } from 'class-validator';
+
+export class CreateCompanyDto {
+    @IsString()
+    company_id: string;
+
+    @IsString()
+    company_name: string;
+
+    @IsString()
+    cnpj: string;
+
+    @IsString()
+    hotel: string;
+    
+    @IsString()
+    products: string;
+}
