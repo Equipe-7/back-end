e2c235910390720cacfecaa307d5d4ce288ab1aa
lucasbanksys <lucs@live.jp>
2021-12-10 19:39:10 -0300

    CRUD donors

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { CompaniesModule } from './companies/companies.module';
 import { CategoriesModule } from './categories/categories.module';
 import { ServicesModule } from './services/services.module';
 import { VetsModule } from './vets/vets.module';
+import { DonorsModule } from './donors/donors.module';
 
 @Module({
-  imports: [UsersModule, ProductsModule, CompaniesModule, CategoriesModule, ServicesModule, VetsModule],
+  imports: [UsersModule, ProductsModule, CompaniesModule, CategoriesModule, ServicesModule, VetsModule, DonorsModule],
   controllers: [],
   providers: [],
 })
---
+++ src/donors/dto/create-donor.dto.ts
@@ -0,0 +1,25 @@
+import { IsEmail, IsString } from 'class-validator';
+
+export class CreateDonorDto {
+  @IsString()
+  donor_id: string;
+
+  @IsString()
+  donor_name: string;
+
+  @IsEmail()
+  @IsString()
+  donor_email: string;
+
+  @IsString()
+  donor_image: string;
+
+  @IsString()
+  donor_species: string;
+
+  @IsString()
+  donor_size: string;
+
+  @IsString()
+  donor_tel: string;
+}
