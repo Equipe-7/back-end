07257d12793e85c2931e5df44819f841ffb163c9
lucasbanksys <lucs@live.jp>
2021-12-10 00:50:01 -0300

    novos estudos do schema

--- src/companies/dto/create-company.dto.ts
+++ src/companies/dto/create-company.dto.ts
@@ -1,18 +1,12 @@
 import { IsString } from 'class-validator';
 
 export class CreateCompanyDto {
-    @IsString()
-    company_id: string;
+  @IsString()
+  company_name: string;
 
-    @IsString()
-    company_name: string;
+  @IsString()
+  cnpj: string;
 
-    @IsString()
-    cnpj: string;
-
-    @IsString()
-    hotel: string;
-    
-    @IsString()
-    products: string;
+  @IsString()
+  hotel: string;
 }
